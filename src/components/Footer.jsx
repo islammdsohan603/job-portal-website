7fc2ba9914ce77965eab5dfa7eb32a4406f0aac6
islammdsohan603 <islammdsohan603@gmail.com>
2026-06-16 18:54:51 +0600

    footer width add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ import { HiLocationMarker } from 'react-icons/hi';
 export default function Footer() {
   return (
     <footer className="bg-slate-50 dark:bg-black border-t border-slate-200 dark:border-white/10">
-      <div className="max-w-7xl mx-auto px-6 lg:px-10 py-16">
+      <div className="w-10/12 mx-auto py-16">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12">
           {/* left side */}
           <div>
